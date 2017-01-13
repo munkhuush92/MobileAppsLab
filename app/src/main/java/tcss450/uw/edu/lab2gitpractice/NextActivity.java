9d0671e3f8b3eefc037bc33f9c7b0bb6814e4964
Munkhbayar Ganbold <king23_gmb@yahoo.com>
2017-01-13 15:56:08 -0800

    added next activity

--- app/src/main/java/tcss450/uw/edu/lab2gitpractice/NextActivity.java
+++ app/src/main/java/tcss450/uw/edu/lab2gitpractice/NextActivity.java
@@ -3,11 +3,11 @@ package tcss450.uw.edu.lab2gitpractice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GoActivity extends AppCompatActivity {
+public class NextActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_go);
+        setContentView(R.layout.activity_next);
     }
 }
